387a70875761081adcb3af95e701a430114d9079
zzinaa <wlsdk2769@ajou.ac.kr>
2022-02-06 19:17:17 +0900

    2562 최대값

---
+++ src/bj2562.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int max = 0;
+        int index = 0;
+
+        for(int i = 0; i < 9; i ++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = 0; i < 9; i ++) {
+            if(max < arr[i]) {
+                max = arr[i];
+                index = i+1;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
